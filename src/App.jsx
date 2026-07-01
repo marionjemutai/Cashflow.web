da034e5578fd4a0db880f4adcde6b19f03ba999a
marionjemutai <yourgithubemail@example.com>
2026-07-01 19:39:33 +0300

    Updated App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
 export default function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
